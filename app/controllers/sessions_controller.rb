e27048836cb2810f16dd86c899f32b3ec9a78e3d
aleksacastle <aaklochko@gmail.com>
2017-10-29 00:41:54 +0300

    def index session controller || route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessioncController < ApplicationController
+  def index
+    @sessions = Session.order(:created_at)
+    respond_to do |format| 
+      format.html
+      format.csv
+    end
+  end
+
   def import
     Session.import(params[:file])
     redirect_to root_url, notice: "File was imported sucessfully"
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :sessions do
     collection { post :import }
   end
+  root "sessions#index"
 end
