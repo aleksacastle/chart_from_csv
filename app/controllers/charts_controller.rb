868c0798eb3fa14a09c394e6e02b518d91a8e762
aleksacastle <aaklochko@gmail.com>
2017-10-30 19:57:18 +0200

    add abnormal failed builds chart || change date format

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,9 +1,9 @@
 class ChartsController < ApplicationController
   def pass_fail
-    render json: Session.group(:summary_status).group_by_day(:created_at).count.chart_json
+    render json: Session.group(:summary_status).group_by_day(:created_at, format: "%e %b").count.chart_json
   end
 
   def duration_time
-    render json: Session.group_by_day(:created_at).sum(:duration).chart_json
+    render json: Session.group_by_day(:created_at, format: "%e %b").sum(:duration).chart_json
   end
 end
